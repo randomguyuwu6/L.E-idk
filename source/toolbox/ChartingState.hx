8c22de0973ead402c83e0d1a8f4a0d102ea612ad
randomguyuwu6 <astolfifemboyuwu4@gmail.com>
2026-08-23 11:39:13 -0300

    Update ChartingState.hx

--- source/toolbox/ChartingState.hx
+++ source/toolbox/ChartingState.hx
@@ -1151,22 +1151,18 @@ class ChartingState extends MusicBeatState {
 		if (section == null)
 			section = curSection;
 
-		if (section <= 0) return 0.0;
-
-		var curBPM:Float = _song.bpm;
-		var totalPos:Float = 0.0;
+		var daBPM:Float = _song.bpm;
+		var daPos:Float = 0;
 
 		for (i in 0...section) {
-			if (_song.notes[i] != null) {
-				if (_song.notes[i].changeBPM && _song.notes[i].bpm > 0) {
-					curBPM = _song.notes[i].bpm;
-				}
-				var steps:Int = _song.notes[i].lengthInSteps > 0 ? _song.notes[i].lengthInSteps : 16;
-				totalPos += (60 / curBPM) * 1000 * (steps / 4);
+			if (_song.notes[i].changeBPM) {
+				daBPM = _song.notes[i].bpm;
 			}
+
+			daPos += Conductor.timeScale[0] * (1000 * (60 / daBPM));
 		}
 
-		return totalPos;
+		return daPos;
 	}
 
 	var beatSnap:Int = 16;
